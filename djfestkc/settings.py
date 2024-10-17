7012e9c5768d81d720e13618d6064e1ebf17e7bf
Ray Doue <rrdoue@gmail.com>
2024-10-17 14:41:54 -0500

    Added the recommended local and production settings file alternative from Arianne Dee's Introduction to Django tutorial on O'Reilly Media.  This change adds the try - except block to include the local_settings.py file if it is present in the same directory.  However, the local_settings.py file is also git-ignored to leave it out of github, so when cloning to a production location, such as pythonanywhere, the block passes since no file is available.

--- djfestkc/settings.py
+++ djfestkc/settings.py
@@ -123,3 +123,8 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
