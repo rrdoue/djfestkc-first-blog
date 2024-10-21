50ee52c98524425ff209458a844a6e80081bcfe3
Ray Doue <rrdoue@gmail.com>
2024-10-21 14:45:28 -0500

    Added functionality to the two urls.py and views.py files to redirect a user from the root location that might normally have an index.html page to a working url.  I'd personally encountered multiple tries testing the application to understand whether or not it was working, only to realize that there is nothing at the `/' location, resulting in `Not Found, The requested resource was not found on this server.`.

--- djfestkc/urls.py
+++ djfestkc/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from djfestkc_blog.views import redirect_view # added as required to call the redirect action below
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', redirect_view) # added to temporarily use a redirect from the root of the site
 ]
--- djfestkc_blog/views.py
+++ djfestkc_blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+# Added to redirect from a usually valid home or index page to a valid url
+def redirect_view(request):
+    response = redirect('/admin')
+    return response
